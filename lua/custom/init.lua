6658b4c8571534cddfdee40ba9b273848497cc14
Samuel Phan <samuel.phan@dilitrust.com>
2025-12-19 18:34:15 +0100

    fix: add auto-insert for terminal

---
+++ lua/custom/init.lua
@@ -0,0 +1,8 @@
+-- Automatically enter insert mode when focusing a terminal window
+vim.api.nvim_create_autocmd({ "WinEnter", "BufWinEnter" }, {
+  pattern = "term://*",
+  callback = function()
+    vim.cmd("startinsert")
+  end,
+})
+
