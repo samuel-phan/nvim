43591faab1ee9b8bef1bb827428c36d58211351d
Samuel Phan <samuel.phan@dilitrust.com>
2026-01-05 14:41:24 +0100

    fix: custom config for whichwrap and clipboard

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,1 +1,10 @@
 require "nvchad.autocmds"
+
+-- Automatically enter insert mode when focusing a terminal window
+vim.api.nvim_create_autocmd({ "WinEnter", "BufWinEnter" }, {
+  pattern = "term://*",
+  callback = function()
+    vim.cmd("startinsert")
+  end,
+})
+
