9f46d58b6426b6160048734587eb2d8c8015e344
Samuel Phan <1516171+samuel-phan@users.noreply.github.com>
2026-01-05 14:41:24 +0100

    fix: custom config for whichwrap and clipboard

--- lua/custom/init.lua
+++
@@ -1,8 +0,0 @@
--- Automatically enter insert mode when focusing a terminal window
-vim.api.nvim_create_autocmd({ "WinEnter", "BufWinEnter" }, {
-  pattern = "term://*",
-  callback = function()
-    vim.cmd("startinsert")
-  end,
-})
-
--- lua/options.lua
+++ lua/options.lua
@@ -1,6 +1,9 @@
 require "nvchad.options"
 
--- add yours here!
+local o = vim.o
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+-- When moving cursor to the right, when at the end of line, do not go to the next line unless for b (backspace) and s (space)
+o.whichwrap = "b,s"
+
+-- Disable VIM registers to copy to the system clipboard (when using `yy`, `dd`, etc.)
+o.clipboard = ""
